2a9b0fb3b04c5aee8da4d3593bc55364a7aa30dc
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-01 22:07:01 -0500

    Added axios interceptor for interacting with backend.

---
+++ src/api/inspiration_v2_api.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000"
+});
+
+api.interceptors.request.use(
+  (request) => {
+    // Add auth token to the authorization header
+    request.headers.authorization = `Bearer ${localStorage.getItem("inspiration_v2_auth_token")}`;
+
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  undefined, // Do nothing if no error in response
+  async (error) => {
+    if (error.response.status == 401) {
+      // Auth was token invalid, so use refresh token to get new auth token.
+
+      let newAuthTokenResponse;
+      try {
+        // Get new auth token
+        newAuthTokenResponse = await axios.post(
+          `${api.defaults.baseURL}/refresh`,
+          undefined,
+          {
+            headers: {
+              authorization: `Bearer ${localStorage.getItem("inspiration_v2_refresh_token")}`
+            }
+          }
+        );
+      } catch (err) {
+        // Refresh token was invalid. User must log in again.
+        return Promise.reject(err);
+      }
+
+      // Store new auth token in local storage
+      localStorage.setItem("inspiration_v2_auth_token", newAuthTokenResponse.data);
+
+      // Use new auth token
+      error.config.headers.authorization = `Bearer ${localStorage.getItem("auth")}`;
+
+      // Re-send request
+      return axios.request(error.config);
+    } else {
+      // Error not from auth token
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default api;
