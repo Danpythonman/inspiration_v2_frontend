e25fae14e6e1c82a7581ea7b53e6fc3d7b97ec47
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-03 17:49:10 -0500

    Added menu on top app bar that appears when settings icon is clicked. It currently has two menu items: log out and change name.

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -1,21 +1,45 @@
 import {
   AppBar,
   Toolbar,
+  Menu,
+  MenuItem,
   Grid,
   IconButton
 } from "@mui/material";
+import { useState } from "react";
 import { Settings } from "@mui/icons-material";
 
 const TopAppBar = () => {
-  return(
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuAnchorElement, setMenuAnchorElement] = useState(null);
+
+  const openMenu = (event) => {
+    setMenuAnchorElement(event.currentTarget);
+    setMenuOpen(true);
+  }
+
+  const closeMenu = () => {
+    setMenuAnchorElement(null);
+    setMenuOpen(false);
+  }
+
+  return (
     <AppBar position="static" color="transparent" elevation={0}>
       <Toolbar>
         <Grid container>
           <Grid item xs={11} />
           <Grid item xs={1}>
-            <IconButton>
+            <IconButton onClick={openMenu}>
               <Settings sx={{ color: "#FFFFFF" }} fontSize="large" />
             </IconButton>
+            <Menu
+              anchorEl={menuAnchorElement}
+              open={menuOpen}
+              onClose={closeMenu}
+            >
+              <MenuItem>Change Name</MenuItem>
+              <MenuItem>Log Out</MenuItem>
+            </Menu>
           </Grid>
         </Grid>
       </Toolbar>
