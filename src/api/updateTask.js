b0bde036c550715778af7dccc6dabdc831820382
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-09 18:59:47 -0500

    Added API request for updating task.

---
+++ src/api/updateTask.js
@@ -0,0 +1,15 @@
+import api from "./inspiration_v2_api";
+
+const updateTask = async (taskId, task) => {
+    const res = await api.put(
+        "/task",
+        {
+            taskId, taskId,
+            task: task
+        }
+    );
+
+    return res;
+}
+
+export default updateTask;
