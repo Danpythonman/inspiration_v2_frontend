624b2fd6fec0c7dde6bdce5efdf6fd387b8d392b
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-09 20:41:59 -0500

    Changed positioning of main message

--- src/components/MessageDisplay.js
+++ src/components/MessageDisplay.js
@@ -1,6 +1,7 @@
 import {
   Box,
   Stack,
+  Grid,
   Typography
 } from "@mui/material";
 
@@ -8,9 +9,15 @@ const MessageDisplay = ({ name, quoteObject }) => {
   return (
     <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
       <Stack spacing={1}>
-        <Typography sx={{ pb: "1rem" }} variant="h2" style={{ color: "#FFFFFF" }}>Welcome {name}</Typography>
-        <Typography style={{ color: "#FFFFFF" }}>{quoteObject.quote}</Typography>
-        <Typography sx={{ pl: "7rem" }} style={{ color: "#FFFFFF" }}>{`- ${quoteObject.author}`}</Typography>
+        <Grid container>
+          <Grid item xs={1} />
+          <Grid item xs={10}>
+            <Typography sx={{ pb: "1rem" }} variant="h2" style={{ color: "#FFFFFF" }}>Welcome {name}</Typography>
+            <Typography style={{ color: "#FFFFFF" }}>{quoteObject.quote}</Typography>
+            <Typography style={{ color: "#FFFFFF" }}>{`- ${quoteObject.author}`}</Typography>
+          </Grid>
+          <Grid item xs={1} />
+        </Grid>
       </Stack>
     </Box>
   );
