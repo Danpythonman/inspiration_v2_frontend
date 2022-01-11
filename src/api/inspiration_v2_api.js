d9083cb361a5d13f18b882bbad96df11589eae32
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-10 21:35:41 -0500

    Changed base URL of API to the deployed backend URL.

--- src/api/inspiration_v2_api.js
+++ src/api/inspiration_v2_api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://inspiration-v2-backend.herokuapp.com"
 });
 
 api.interceptors.request.use(
