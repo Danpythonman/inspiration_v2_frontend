e52fb7af715cff4d9e24e7ac14dd1c6f96787893
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-01-10 22:09:39 -0500

    Changed spacing of bottom app bar so that info and credit button and to-do list button are on left and right ends of the screen, respectively. Also removed image and quote credit on the appbar.

--- src/components/BottomAppBar.js
+++ src/components/BottomAppBar.js
@@ -1,11 +1,7 @@
 import {
   AppBar,
   Toolbar,
-  Grid,
-  Stack,
-  Button,
-  Link,
-  Typography
+  Button
 } from "@mui/material";
 import { useState } from "react";
 import TodoListFullScreen from "./TodoListFullScreen";
@@ -26,39 +22,23 @@ const BottomAppBar = ({ tasks, updateTasks, imageObject, quoteObject }) => {
   return (
     <AppBar position="fixed" color="transparent" elevation={0} sx={{ top: "auto", bottom: 0 }}>
       <Toolbar>
-        <Grid container>
-          <Grid item xs={2}>
-            <Button
-              variant="text"
-              color="info"
-              style={{ borderRadius: 1, backgroundColor: "transparent", position: "fixed", left: "1rem" }}
-              onClick={openSiteInfoDialog}
-            >
-              <strong>Info and Credit</strong>
-            </Button>
-          </Grid>
-          <Grid item xs={2} />
-          <Grid item xs={4}>
-            <Button
-              variant="text"
-              color="info"
-              style={{ borderRadius: 1, backgroundColor: "transparent" }}
-              onClick={openTodoList}
-            >
-              <strong>To-do List</strong>
-            </Button>
-          </Grid>
-          <Grid item xs={4}>
-            <Stack style={{ position: "fixed", right: 5, bottom: 5 }} alignItems="flex-end">
-            <Typography style={{ color: "#A9A9A9" }}>
-              Image Courtesy of <Link color="inherit" underline="hover" href="https://data.nasa.gov/Space-Science/Astronomy-Picture-of-the-Day-API/ez2w-t8ua">NASA's Astronomy Picture of the Day API</Link>
-            </Typography>
-            <Typography style={{ color: "#A9A9A9" }}>
-              Quote Courtesy of <Link color="inherit" underline="hover" href="https://quotes.rest/">They Said So Quotes API</Link>
-            </Typography>
-            </Stack>
-          </Grid>
-        </Grid>
+        <Button
+          variant="text"
+          color="info"
+          style={{ borderRadius: 1, backgroundColor: "transparent" }}
+          onClick={openSiteInfoDialog}
+        >
+          <strong>Info and Credit</strong>
+        </Button>
+        <div style={{ flex: 1 }}></div>
+        <Button
+          variant="text"
+          color="info"
+          style={{ borderRadius: 1, backgroundColor: "transparent" }}
+          onClick={openTodoList}
+        >
+          <strong>To-do List</strong>
+        </Button>
       </Toolbar>
       <TodoListFullScreen
         open={todoListOpen}
